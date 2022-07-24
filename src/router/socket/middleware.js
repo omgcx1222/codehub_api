5b499b34d19542ae4dcd3e2d6031fa84e24af654
itcast <hello@itcast.cn>
2022-07-24 23:31:26 +0800

    socket群聊私聊

--- src/router/socket/middleware.js
+++ src/router/socket/middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 const { PUBLIC_KEY } = require("../../app/config")
-const { createChatRecord, selectChatRecord } = require("./serviec")
+const { createChatRecord, selectChatRecord, createChatRoom, roomAddUser } = require("./serviec")
 const createUid = require("../../util/createUid")
 
 // 验证身份
@@ -34,45 +34,123 @@ function verify(userInfo) {
 
 // 给所有人发送
 function allSend(data) {
-  onLineUsers.forEach((user) => {
-    user?.ctx?.websocket?.send(JSON.stringify(data))
+  Object.keys(onLineUsers).forEach((user) => {
+    // console.log(data)
+    onLineUsers[user]?.ctx?.websocket?.send(JSON.stringify(data))
   })
 }
 
-// 指定给某人发送
+/**
+ * 指定给某人发送
+ * @param {*} ctx 指定发送的那个人
+ * @param {*} data
+ */
 function assignSend(ctx, data) {
-  ctx.websocket.send(JSON.stringify(data))
+  ctx?.websocket?.send(JSON.stringify(data))
 }
 
-// 存储所有在线用户的ctx
-const onLineUsers = new Map()
+// 获取用户信息
+function getOnLineInfo() {
+  let c1 = 0 // 游客在线人数
+  let c2 = 0 // 登录用户在线个数
+  Object.keys(onLineUsers).forEach((item) => {
+    onLineUsers[item]?.userInfo?.type ? c1++ : c2++
+  })
+
+  return {
+    touristCount: c1,
+    loginCount: c2
+  }
+}
+
+// 存储所有在线用户
+// const onLineUsers = new Map()
+const onLineUsers = {}
 
 class SocketMiddleware {
   async connectSocket(ctx, next) {
     const uid = createUid()
-    onLineUsers.set(uid)
     let currentUser = {}
-
+    onLineUsers[uid] = { ctx, userInfo: currentUser }
     // 接收
     ctx.websocket.on("message", async (msg) => {
       const { type = "", data = {} } = JSON.parse(msg)
-      const { userInfo = {} } = data
+      /**
+       * userInfo 必传（token）
+       * message  发送信息时必传
+       * chatId   群聊时必传
+       * userId   私聊时必传
+       */
+      const { userInfo = {}, message = "", chatId, userId } = data
+
       currentUser = verify(userInfo)
-      onLineUsers.set(uid, { ctx, userInfo: currentUser })
+      // 游客默认加入 id为1 的聊天群
+      currentUser.chatRoomIds = onLineUsers[uid].userInfo?.chatRoomIds ?? [1]
+      // 内存地址赋值
+      onLineUsers[uid].userInfo = currentUser
+      // console.log(currentUser === onLineUsers[uid].userInfo)
 
       switch (type) {
         case "login":
-          allSend({ type: "online", data: { onLineCount: onLineUsers.size, onLineUser: currentUser } })
-          break
-        case "chatMessage":
-          const { message = "" } = data
-          // 登录用户
+          let chatRooms = []
           if (!currentUser.type) {
-            // 内容不能为空
-            if (message) {
-              const result = await createChatRecord(currentUser.id, message)
-              const result2 = await selectChatRecord(result.insertId)
-              allSend({ type: "userMessage", data: { onLineCount: onLineUsers.size, chatRecord: result2[0] } })
+            chatRooms = await selectChatRecord(currentUser.id)
+            currentUser.chatRoomIds = chatRooms.map((item) => item.id)
+            // onLineUsers保存着currentUser的地址，所以这一步可以注释掉
+            // onLineUsers[uid].userInfo = currentUser
+          } else {
+            chatRooms = await selectChatRecord()
+          }
+          assignSend(ctx, { type: "chatRecord", data: { chatRooms } })
+          allSend({ type: "onLine", data: { onLineCount: getOnLineInfo(), onLineUser: currentUser } })
+          break
+        case "sendPublicChat":
+          if (!currentUser.type) {
+            // 内容和群聊id不能为空
+            if (message && chatId) {
+              const result = await createChatRecord(currentUser.id, chatId, message)
+              // 给所有聊天室成员发送这条信息
+              Object.keys(onLineUsers).forEach((uid) => {
+                const u = onLineUsers[uid].userInfo.chatRoomIds?.find((id) => id == chatId)
+                // console.log(onLineUsers[uid].userInfo, chatId)
+                if (u) {
+                  const { id, nickname, avatarUrl = null } = currentUser
+                  assignSend(onLineUsers[uid].ctx, {
+                    type: "sendChat",
+                    data: {
+                      chatMessage: { id: result.insertId, message, userId: id, nickname, avatarUrl: avatarUrl }
+                    }
+                  })
+                }
+              })
+              // allSend({ type: "sendChat", data: { chatMessage: result2[0] } })
+            }
+          }
+          break
+        // 私聊
+        case "sendPrivateChat":
+          if (!currentUser.type) {
+            // 内容和用户id不能为空
+            if (message && userId) {
+              // 创建聊天室
+              const room = await createChatRoom("私聊")
+              // 添加聊天室成员
+              await roomAddUser(currentUser.id, room.insertId)
+              await roomAddUser(userId, room.insertId)
+              // 保存消息记录
+              const result = await createChatRecord(currentUser.id, room.insertId, message)
+
+              // 给私聊的对象（登录在线）发送消息
+              Object.keys(onLineUsers).some((user) => {
+                const u = onLineUsers[user].userInfo.chatRoomIds.find((id) => id == chatId)
+                if (u) {
+                  const { id, nickname, avatarUrl } = currentUser
+                  assignSend(onLineUsers[user].ctx, {
+                    type: "sendChat",
+                    data: { chatMessage: { id: result.insertId, message, userId: id, nickname, avatarUrl } }
+                  })
+                }
+              })
             }
           }
         default:
@@ -82,8 +160,8 @@ class SocketMiddleware {
 
     // 关闭
     ctx.websocket.on("close", async (state) => {
-      onLineUsers.delete(uid)
-      allSend({ type: "dropLine", data: { onLineCount: onLineUsers.size } })
+      delete onLineUsers[uid]
+      allSend({ type: "dropLine", data: { onLineCount: getOnLineInfo() } })
     })
   }
 }
--- src/router/socket/serviec.js
+++ src/router/socket/serviec.js
@@ -1,11 +1,11 @@
-const connection = require('../../app/database')
+const connection = require("../../app/database")
 
 class SocketService {
   // 创建记录
-  async createChatRecord(id, content) {
-    const statement = "INSERT INTO chat_record (user_id, content) VALUES (?, ?)"
+  async createChatRecord(id, chatId, message) {
+    const statement = "INSERT INTO chat_list (user_id, chat_id, message) VALUES (?, ?, ?)"
     try {
-      const [result] = await connection.execute(statement, [id, content])
+      const [result] = await connection.execute(statement, [id, chatId, message])
       return result
     } catch (error) {
       return error.message
@@ -15,28 +15,55 @@ class SocketService {
   // 查询记录
   async selectChatRecord(id) {
     try {
-      let statement = ''
-      if(id) {
-        statement = `
-          SELECT cr.content, cr.createTime,
-            JSON_OBJECT('id', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url) user
-          FROM chat_record cr 
-          LEFT JOIN users u ON cr.user_id = u.id
-          WHERE cr.id = ?
+      // 登录用户
+      if (id) {
+        const statement = `
+          SELECT cu.chat_id id, c.name,
+            JSON_ARRAYAGG(JSON_OBJECT('id', cl.id, 'message', cl.message, 'userId', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url)) chats
+          FROM chats_users cu
+          RIGHT JOIN chats_list cl ON cl.chat_id = cu.chat_id
+          LEFT JOIN chats c ON c.id = cu.chat_id
+          LEFT JOIN users u ON u.id = cu.user_id 
+          WHERE cu.user_id = ?
+          GROUP BY cu.chat_id
         `
         const [result] = await connection.execute(statement, [id])
         return result
-      }else {
-        statement = `
-          SELECT cr.content, cr.createTime,
-            JSON_OBJECT('id', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url) user
-          FROM chat_record cr 
-          LEFT JOIN users u ON cr.user_id = u.id
-          ORDER BY cr.createTime
+      } else {
+        const statement = `
+          SELECT cl.chat_id id, c.name,
+            JSON_ARRAYAGG(JSON_OBJECT('id', cl.id, 'message', cl.message, 'userId', u.id, 'nickname', u.nickname, 'avatarUrl', u.avatar_url)) chats
+          FROM chats_list cl
+          LEFT JOIN chats c ON c.id = cl.chat_id
+          LEFT JOIN users u ON u.id = cl.user_id
+          WHERE c.name = '正能量聊天群'
+          GROUP BY cl.chat_id
         `
         const [result] = await connection.execute(statement)
         return result
       }
+    } catch (error) {
+      return error.message
+    }
+  }
+
+  // 创建聊天室
+  async createChatRoom(name) {
+    const statement = "INSERT INTO chats (name) VALUES (?)"
+    try {
+      const [result] = await connection.execute(statement, [name])
+      return result
+    } catch (error) {
+      return error.message
+    }
+  }
+
+  // 添加聊天室成员
+  async roomAddUser(userId, roomId) {
+    const statement = "INSERT INTO chats_users (user_id, chat_id) VALUES (?, ?)"
+    try {
+      const [result] = await connection.execute(statement, [userId, roomId])
+      return result
     } catch (error) {
       return error.message
     }
